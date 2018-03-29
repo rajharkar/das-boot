27179609706cc6c961e10a552e3461228a515019
rajha <rajha@DESKTOP-64M9EUE>
2018-03-29 00:54:39 -0400

    c

--- src/main/java/com/boot/Application.java
+++ src/main/java/com/boot/Application.java
@@ -1,17 +1,22 @@
 package com.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-public class App 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application 
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args );
+        SpringApplication.run(Application.class, args );
     }
 }
